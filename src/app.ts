2e823685b45caa81fd8b5abd25ee5ea1e6187687
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-07-23 18:36:01 -0300

    Express config

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("Deu certo!")
+import express from 'express'
+import config from 'config'
+
+const app = express()
+
+app.use(express.json())
+
+const port = 3000
+
+app.listen(port, async () => {
+    console.log(`Application running on port: ${port}`)
+})
