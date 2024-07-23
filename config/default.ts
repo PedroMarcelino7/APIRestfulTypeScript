0a7d14a89a89cb03280d79a0165fda407ab3280f
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-07-23 18:55:27 -0300

    Creating mongoose string connection

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,4 @@
 export default {
-    port: 3000
+    port: 3000,
+    dbUri: "mongodb+srv://pedropmarcelino2004:<password>@cluster0.mcamf3t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 }
