9efbd5ab615b621b54a3aa24c5bbd58c9797e012
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-07-24 13:40:33 -0300

    Setting morgan middleware

--- config/logger.ts
+++ config/logger.ts
@@ -1,20 +1,20 @@
-import winston from "winston"
-import config from "config"
+import winston from "winston";
+import config from "config";
 
 const levels = {
     error: 0,
     warn: 1,
     info: 2,
     http: 3,
-    debug: 4
-}
+    debug: 4,
+};
 
 const level = () => {
-    const env = config.get<string>("env") || "development"
-    const isDevelopment = env === "development"
+    const env = config.get<string>("env") || "development";
+    const isDevelopment = env === "development";
 
-    return isDevelopment ? "debug" : "warn"
-}
+    return isDevelopment ? "debug" : "warn";
+};
 
 const colors = {
     error: "red",
@@ -22,9 +22,9 @@ const colors = {
     info: "green",
     http: "magenta",
     debug: "white",
-}
+};
 
-winston.addColors(colors)
+winston.addColors(colors);
 
 const format = winston.format.combine(
     winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
@@ -32,7 +32,7 @@ const format = winston.format.combine(
     winston.format.printf(
         (info) => `${info.timestamp} - ${info.level}: ${info.message}`
     )
-)
+);
 
 const transports = [
     new winston.transports.Console(),
@@ -41,13 +41,13 @@ const transports = [
         level: "error"
     }),
     new winston.transports.File({ filename: "logs/all.log" })
-]
+];
 
 const Logger = winston.createLogger({
     level: level(),
     levels,
     format,
     transports
-})
+});
 
-export default Logger
+export default Logger;
--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,12 @@ import config from 'config'
 import router from './router'
 import db from '../config/db'
 import Logger from '../config/logger'
+import morganMiddleware from './middleware/morganMiddleware'
 
 const app = express()
 
 app.use(express.json())
-
+app.use(morganMiddleware)
 app.use("/api/", router)
 
 const port = config.get<number>('port')
