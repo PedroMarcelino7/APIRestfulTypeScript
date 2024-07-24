77ba6c7a23c3be47827af8e5e747f916667d2065
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-07-24 16:07:42 -0300

    Updating movie data

--- src/controllers/movieControllers.ts
+++ src/controllers/movieControllers.ts
@@ -54,3 +54,21 @@ export async function removeMovie(req: Request, res: Response) {
         Logger.error("Error: ", error.message)
     }
 }
+
+export async function updateMovie(req: Request, res: Response) {
+    try {
+        const id = req.params.id
+        const data = req.body.data
+        const movie = await MovieModel.findById(id)
+
+        if (!movie) {
+            return res.status(404).json({ error: "Filme não encontrado." })
+        }
+
+        await MovieModel.updateOne({ _id: id }, data)
+
+        return res.status(200).json(data)
+    } catch (error: any) {
+        Logger.error("Error: ", error.message)
+    }
+}
