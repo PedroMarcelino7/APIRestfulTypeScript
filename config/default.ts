4e29224ef9c1608358f533fbeeac60650813052a
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-07-23 21:00:00 -0300

    Creating winston logs

--- config/default.ts
+++ config/default.ts
@@ -3,5 +3,6 @@ const dbPass = process.env.DB_PASS
 
 export default {
     port: 3000,
-    dbUri: `mongodb+srv://${dbUser}:${dbPass}@cluster0.mcamf3t.mongodb.net/APIRestfulTS?retryWrites=true&w=majority&appName=Cluster0`
+    dbUri: `mongodb+srv://${dbUser}:${dbPass}@cluster0.mcamf3t.mongodb.net/APIRestfulTS?retryWrites=true&w=majority&appName=Cluster0`,
+    env: "development"
 }
